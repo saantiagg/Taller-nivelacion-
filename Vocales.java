b6339f0205e3b3d3d689793937697f783fb409fe
Saantiagg <santimunoz68@gmail.com>
2025-08-21 16:42:38 -0500

    Codigo en funcionamiento

--- Vocales.java
+++ Vocales.java
@@ -11,5 +11,35 @@ public class Vocales {
         int vocales = 0;
         int consonantes = 0;
 
+        for (int i = 0; i < palabra.length(); i++) {
+            char letra  = palabra.charAt(i);
+
+            if (esLetra(letra)) {
+                if (esVocal(letra)) {
+                    vocales++;
+                } else {
+                    consonantes++;
+                }
+            }
+        }
+
+        System.out.println("\nCantidad de vocales: " + vocales);
+        System.out.println("Cantidad de consonantes: " + consonantes);
+
+        sc.close();
     }
-}
+
+    public static boolean esLetra(char  c) {
+        return c >= 'a' && c <= 'z';
+    }
+    public static boolean esVocal(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+
+    }
+
+
+   }
+        
+    
+    
+
