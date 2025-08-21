2030e243a16da39ddb4b48052ccfbb82ea33a737
Saantiagg <santimunoz68@gmail.com>
2025-08-21 16:10:46 -0500

    Vocales

---
+++ Vocales.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Vocales {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Ingrese una palabra en minusculas: ");
+        String palabra = sc.nextLine();
+
+        int vocales = 0;
+        int consonantes = 0;
+
+    }
+}
